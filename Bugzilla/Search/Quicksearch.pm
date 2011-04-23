5cb1678e01c9166baf8d8821dbffba2a9e1fe07b
Christian Ruppert <idl0r@gentoo.org>
2011-04-24 00:49:53 +0200

    Use short_desc/summary by default in quicksearch
    instead of product, component, keywords, status_whiteboard and content.

--- Bugzilla/Search/Quicksearch.pm
+++ Bugzilla/Search/Quicksearch.pm
@@ -473,23 +473,23 @@ sub _special_field_syntax {
 sub _default_quicksearch_word {
     my ($word, $negate) = @_;
     
-    if (!grep { lc($word) eq $_ } PRODUCT_EXCEPTIONS and length($word) > 2) {
-        addChart('product', 'substring', $word, $negate);
-    }
+#    if (!grep { lc($word) eq $_ } PRODUCT_EXCEPTIONS and length($word) > 2) {
+#        addChart('product', 'substring', $word, $negate);
+#    }
     
-    if (!grep { lc($word) eq $_ } COMPONENT_EXCEPTIONS and length($word) > 2) {
-        addChart('component', 'substring', $word, $negate);
-    }
+#    if (!grep { lc($word) eq $_ } COMPONENT_EXCEPTIONS and length($word) > 2) {
+#        addChart('component', 'substring', $word, $negate);
+#    }
     
-    my @legal_keywords = map($_->name, Bugzilla::Keyword->get_all);
-    if (grep { lc($word) eq lc($_) } @legal_keywords) {
-        addChart('keywords', 'substring', $word, $negate);
-    }
+#    my @legal_keywords = map($_->name, Bugzilla::Keyword->get_all);
+#    if (grep { lc($word) eq lc($_) } @legal_keywords) {
+#        addChart('keywords', 'substring', $word, $negate);
+#    }
     
-    addChart('alias', 'substring', $word, $negate);
+#    addChart('alias', 'substring', $word, $negate);
     addChart('short_desc', 'substring', $word, $negate);
-    addChart('status_whiteboard', 'substring', $word, $negate);
-    addChart('content', 'matches', _matches_phrase($word), $negate);
+#    addChart('status_whiteboard', 'substring', $word, $negate);
+#    addChart('content', 'matches', _matches_phrase($word), $negate);
 }
 
 sub _handle_urls {
