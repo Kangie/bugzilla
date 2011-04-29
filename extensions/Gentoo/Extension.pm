49faa1f71010611d516d392252f8726b085b0cd4
Christian Ruppert <idl0r@gentoo.org>
2011-04-29 14:30:07 +0200

    Set gentoo specific permissions via hook
    Since 4.0.1 its possible to fix permissions via hook instead of modifying the
    Bugzilla/Install/Filesystem.pm.

--- extensions/Gentoo/Extension.pm
+++ extensions/Gentoo/Extension.pm
@@ -2,9 +2,36 @@ package Bugzilla::Extension::Gentoo;
 use strict;
 use base qw(Bugzilla::Extension);
 
+use Bugzilla::Install::Filesystem qw(CGI_READ OWNER_EXECUTE WS_SERVE DIR_WS_SERVE);
+use Bugzilla::Constants qw(bz_locations);
+
 use POSIX;
 
 our $VERSION = '1.0';
+
+sub install_filesystem {
+	my $datadir = bz_locations()->{'datadir'};
+
+	my %files = (
+		"zzz.txt"        => { perms => CGI_READ },
+		"robots-ssl.txt" => { perms => CGI_READ },
+		"bots.html"      => { perms => CGI_READ },
+		"favicon.ico"    => { perms => CGI_READ },
+		"runstats.sh"    => { perms => OWNER_EXECUTE },
+		"recompile.sh"   => { perms => OWNER_EXECUTE },
+    );
+
+	my %recurse_dirs = (
+		"$datadir/cached" => { files => WS_SERVE, dirs => DIR_WS_SERVE },
+	);
+
+	my %htaccess = (
+		"$datadir/cached/.htaccess" => { perms => WS_SERVE, contents => <<EOT
+# Allow access to the cached stuff
+Allow from all
+EOT
+	);
+}
 
 sub template_before_create {
 	my ($self, $args) = @_;
