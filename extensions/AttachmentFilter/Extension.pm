d1ca3da748592b0d00d5ff53652a9347b7daf430
Max Magorsch <arzano@gentoo.org>
2020-05-08 21:53:03 +0200

    AttachmentFilter: Exclude php files as well
    Signed-off-by: Max Magorsch <arzano@gentoo.org>

--- extensions/AttachmentFilter/Extension.pm
+++ extensions/AttachmentFilter/Extension.pm
@@ -12,14 +12,17 @@ use strict;
 use base qw(Bugzilla::Extension);
 use Bugzilla::Error;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 ################################################################################
 # This extension is filtering attachments. Currently its is filtering html
 # attachments.
 ################################################################################
 sub attachment_process_data {
   my ($self, $args) = @_;
-  return unless ( ($args->{attributes}->{mimetype} eq 'text/html') or ($args->{attributes}->{filename} =~ /\.htm\z/) or ($args->{attributes}->{filename} =~ /\.html\z/) );
+  return unless ($args->{attributes}->{mimetype} eq 'text/html') or
+                ($args->{attributes}->{filename} =~ /\.htm\z/) or
+                ($args->{attributes}->{filename} =~ /\.html\z/) or
+                ($args->{attributes}->{filename} =~ /\.php\z/);
 
   ${$args->{data}} = '';
   $args->{attributes}->{filename} = '';
